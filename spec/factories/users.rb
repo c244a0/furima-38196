03ba6a5f6e73fc5aa72665461691fa86da71888d
c244a0 <jadegqu175@gmail.com>
2022-07-18 23:05:50 +0900

    ユーザー管理機能の実装

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {Faker::Name.last_name}
+    email                 {Faker::Internet.free_email}
+    password              {'tester123'}
+    password_confirmation {password}
+    last_name             { '佐藤' }
+    first_name            { 'ひろし' }
+    last_name_kana        { 'サトウ' }
+    first_name_kana       { 'ヒロシ' }
+    birthday              {Faker::Date.between_except(from: 1.year.ago, to: 1.year.from_now, excepted: Date.today)}
+  end
+end
